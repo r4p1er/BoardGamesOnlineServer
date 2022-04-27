ecf78146907fe44df020f3ed62772e8054228b92
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2022-04-27 21:10:38 +0300

    Refactored TickTackToePlayer and TickTackToeHub like ShipBattle stuff is

--- BoardGamesOnline.Server/Games/TickTackToePlayer.cs
+++ BoardGamesOnline.Server/Games/TickTackToePlayer.cs
@@ -1,28 +1,89 @@
 ﻿namespace BoardGamesOnline.Server.Games
 {
-    public class TickTackToePlayer
+    public class TickTackToePlayer : IDisposable
     {
         private string me;
-        private string opponent;
-        private TickTackToe game;
-        private bool right;
+        private string? opponent;
+        private TickTackToe? game;
+        private bool? turn;
+        private Action abort;
+        private CancellationTokenSource cancellationTokenSource;
 
-        public TickTackToePlayer(string me, string opponent, TickTackToe game, bool right)
+        public TickTackToePlayer(string me, Action abort)
         {
             this.me = me;
-            this.opponent = opponent;
-            this.game = game;
-            this.right = right;
+            this.abort = abort;
+            this.cancellationTokenSource = new CancellationTokenSource();
         }
 
-        public void SwitchRight()
+        public void Abort()
         {
-            right = !right;
+            abort();
+        }
+
+        public void AbortAfterSeconds(int seconds)
+        {
+            var token = cancellationTokenSource.Token;
+            Task.Run(async () =>
+            {
+                bool cancelled = false;
+                token.Register(() => cancelled = true);
+                await Task.Delay(1000 * seconds);
+                if (cancelled) return;
+                Abort();
+            }, token);
+        }
+
+        public void CancelAbortion()
+        {
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+            cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public void SwitchTurn()
+        {
+            turn = !turn;
+        }
+
+        public void Dispose()
+        {
+            cancellationTokenSource.Dispose();
         }
 
         public string Me { get { return me; } }
-        public string Opponent { get { return opponent; } }
-        public TickTackToe Game { get { return game; } }
-        public bool Right { get { return right; } }
+        public string? Opponent
+        {
+            get
+            {
+                return opponent;
+            }
+            set
+            {
+                if (opponent == null) opponent = value;
+            }
+        }
+        public TickTackToe? Game
+        {
+            get
+            {
+                return game;
+            }
+            set
+            {
+                if (game == null) game = value;
+            }
+        }
+        public bool? Turn
+        {
+            get
+            {
+                return turn;
+            }
+            set
+            {
+                if (turn == null) turn = value;
+            }
+        }
     }
 }
