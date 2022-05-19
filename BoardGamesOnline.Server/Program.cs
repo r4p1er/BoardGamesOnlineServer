b779e3e47357e9dd938e7a8d84caf286b70434d3
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2022-05-19 22:42:44 +0300

    App is configured so to work with a reverse proxy

--- BoardGamesOnline.Server/Program.cs
+++ BoardGamesOnline.Server/Program.cs
@@ -1,13 +1,21 @@
 using BoardGamesOnline.Server.Games;
 using BoardGamesOnline.Server.Hubs;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<TickTackToeQueue>();
 builder.Services.AddSingleton<TickTackToeInfo>();
 builder.Services.AddSingleton<ShipBattleQueue>();
 builder.Services.AddSingleton<ShipBattleInfo>();
+
 var app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.MapGet("/", () => "Runs");
 
